ad21e1d3b86411f3f7176c99404cae38afdf38f4
lhish <lhy770422@163.com>
2026-01-27 19:01:18 +0800

    chore: bump to v1.2.0

--- Casks/cloudyield.rb
+++ Casks/cloudyield.rb
@@ -1,6 +1,6 @@
 cask "cloudyield" do
-  version "1.1.1"
-  sha256 "c660a6cce093cb1a54c758216df4eed76e31acf20e9dc347f7ab300d4604e085"
+  version "1.2.0"
+  sha256 "6dde1121732bf2da48e53b06ade2ac26f018f68226bae0e71dcfdad97ad902a0"
 
   url "https://github.com/lhish/CloudYield/releases/download/v#{version}/CloudYield-#{version}.zip"
   name "CloudYield"
