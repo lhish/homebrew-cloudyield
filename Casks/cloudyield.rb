4c24d918f85d3bdc9f7b61204d6cd49961110419
lhish <lhy770422@163.com>
2026-01-29 12:34:12 +0800

    chore: update cloudyield to 1.3.0

--- Casks/cloudyield.rb
+++ Casks/cloudyield.rb
@@ -1,6 +1,6 @@
 cask "cloudyield" do
-  version "1.2.0"
-  sha256 "6dde1121732bf2da48e53b06ade2ac26f018f68226bae0e71dcfdad97ad902a0"
+  version "1.3.0"
+  sha256 "cd4e665cec879bcb59689be876ad1bedc85df746681e1a6abdbc1cfa77f6f50e"
 
   url "https://github.com/lhish/CloudYield/releases/download/v#{version}/CloudYield-#{version}.zip"
   name "CloudYield"
